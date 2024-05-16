f6f03b0f6861959aa5a9f0468c2655a3a4607ecb
Keith Muller <66692457+keithmuller@users.noreply.github.com>
2024-05-16 08:26:37 -0700

    Update Comments

--- Cmain.c
+++ Cmain.c
@@ -16,32 +16,43 @@
 
 // version 1.1 05/08/2024
 
+/*
+ * cipher program
+ * usage:  cipher 9-d|-s) -b bookfile
+ * cipher reads the data to be processed from standard input, either
+ * encrypts (-e) or decrypts (-d) the data, and then writes the result
+ * to standard output.
+ */
+
 int
 main(int argc, char **argv)
 {
     /*
-     * local vars on stack
+     * local vars on stack these are given to help you write main.S later
+     * it is suggested that you not change the order of the vars.
      */
     FILE *fpbook;                   // File * to bookfile
-    unsigned char inbuf[BUFSZ];     // input buffer
-    unsigned char bookbuf[BUFSZ];   // output buffer
+    unsigned char inbuf[BUFSZ];     // input buffer - same size as bookbuf
+    unsigned char bookbuf[BUFSZ];   // output buffer - same size as inbuf
     int (*func)();                  // function pointer to ecrypt or dcrypt
     char *argv0 = *argv;            // saved argv[0] for fprintf()
-    // end of stack variables
-
-    int cnt;            // does not need to be on stack (used as r0)
+    // end of required stack variables
 
     /*
-     * parse the command line arguments, set mode (encrypt or decrypt)
-     * and open the book file
+     * call setup which does:
+     * (1) parse the command line arguments, (2) func is set to point at either
+     * ecrypt or dcrypt and open the book file
      */
     if (setup(argc, argv, &func, &fpbook) == RETRN_FAIL)
         return EXIT_FAILURE;
 
     /*
-     * read the input and book file until EOF on the input file
-     * Either func points at encrypt or decrypt
-     * then write it out.
+     * (1) use rdbuf() to read the input from stdin into inbuf and read 
+     * the book file into bookbuf until EOF on the input file. Pass
+     * rdbuf() BUFSZ as the size of the buffers 
+     * (2) Call func to encrypt or decrypt
+     * (3) use fwrite() to write inbuf to stdout. Make sure to check that
+     *  fwrite() writes all the valid contents contained in inbuf.
      */ 
     //
     // complete the io loop
@@ -50,13 +61,17 @@ main(int argc, char **argv)
     // fprintf(stderr, "%s: read failed\n", argv0);
     // return EXIT_FAILURE;
     // 
-    // if fwrite fails to write all the chars rdbuf() returns then
+    // if fwrite fails to write all the chars rdbuf() says are in innbuf,
+    // then
     // fprintf(stderr, "%s: write failed\n", argv0);
     // (void)fclose(fpbook);
     // return EXIT_FAILURE; 
+    // end of loop
+
     //
     //  do not forget to close the book file
     //  (void)fclose(fpbook);
+    //  return EXIT_FAILURE if anything failed to operate properly
     //
 
     return EXIT_SUCCESS;
